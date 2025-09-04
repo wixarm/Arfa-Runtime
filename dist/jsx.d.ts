38a1a6fb6c7150c6a7fa45f794921889c08ad6b8
wixarm <armantarhani1997@gmail.com>
2025-09-04 12:30:20 +0330

    dist updated

--- dist/jsx.d.ts
+++ dist/jsx.d.ts
@@ -1,11 +1,7 @@
-interface VNode {
+export declare function h(type: string | Function, props: Record<string, any> | null, ...children: any[]): {
     type: string | Function;
     props: Record<string, any>;
-    key?: string | number | null;
-    ref?: any;
-}
-export declare function h(type: string | Function, props: Record<string, any> | null, ...children: any[]): VNode;
+};
 export declare function Fragment(props: {
     children?: any;
 }): any;
-export {};
--- dist/jsx.js
+++ dist/jsx.js
@@ -1,27 +1,8 @@
 export function h(type, props, ...children) {
-    const normalizedProps = props ? { ...props } : {};
-    let key = null;
-    let ref = null;
-    if (normalizedProps.hasOwnProperty("key")) {
-        key = normalizedProps.key;
-        delete normalizedProps.key;
-    }
-    if (normalizedProps.hasOwnProperty("ref")) {
-        ref = normalizedProps.ref;
-        delete normalizedProps.ref;
-    }
-    const flatChildren = children.flat(Infinity);
-    const filteredChildren = flatChildren.filter((child) => child != null && child !== false && child !== true);
-    normalizedProps.children =
-        filteredChildren.length === 1 ? filteredChildren[0] : filteredChildren;
-    const vnode = { type, props: normalizedProps };
-    if (key !== null) {
-        vnode.key = key;
-    }
-    if (ref !== null) {
-        vnode.ref = ref;
-    }
-    return vnode;
+    props = props ? { ...props } : {};
+    const flat = children.flat().filter((c) => c != null);
+    props.children = flat.length === 1 ? flat[0] : flat;
+    return { type, props };
 }
 export function Fragment(props) {
     return props.children;
